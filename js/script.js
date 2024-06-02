7973e3019edd6d1fa8a4c16c5ec3debc58f20022
Nazar Dzuba <nazardzuba59@gmail.com>
2024-06-02 17:19:56 +0300

    commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+// IBG
+function ibg() {
+  let ibg = document.querySelectorAll(".ibg");
+  for (var i = 0; i < ibg.length; i++) {
+    if (ibg[i].querySelector("img")) {
+      ibg[i].style.backgroundImage =
+        "url(" + ibg[i].querySelector("img").getAttribute("src") + ")";
+    }
+  }
+}
+ibg();
+
+let sections = document.querySelectorAll("section");
+
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 400;
+    let height = sec.offsetHeight;
+
+    if (top >= offset && top < offset + height) {
+      sec.classList.add("show-animate");
+    } else {
+      sec.classList.remove("show-animate");
+    }
+  });
+};
